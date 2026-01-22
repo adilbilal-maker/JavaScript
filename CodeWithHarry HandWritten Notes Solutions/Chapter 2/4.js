4de8ea5d92782dc53106c45cfdd5b7f1a40c9669
adilbilal-maker <adilbilal453@gmail.com>
2026-01-22 14:53:30 +0500

    Add divisibility check for 2 or 3 in 4.js

---
+++ CodeWithHarry HandWritten Notes Solutions/Chapter 2/4.js
@@ -0,0 +1,7 @@
+let number = 9;
+
+if (number % 2 === 0 || number % 3 === 0) {
+    console.log(number + " is divisible by 2 or 3.");
+} else {
+    console.log(number + " is not divisible by 2 or 3.");
+}
