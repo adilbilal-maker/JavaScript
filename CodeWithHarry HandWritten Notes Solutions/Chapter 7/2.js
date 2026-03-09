aba68c639f47179ccd02f56da8160b90eea57669
adilbilal-maker <adilbilal453@gmail.com>
2026-03-09 22:03:51 +0500

    Add HTML table displaying name and age

---
+++ CodeWithHarry HandWritten Notes Solutions/Chapter 7/2.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<table border="1">
+    <tr>
+        <td>Name</td>
+        <td>Age</td>
+    </tr>
+    <tr>
+        <td>Harry</td>
+        <td>20</td>
+    </tr>
+</table>
+
+</body>
+</html>
