fa7a85cf2059b1fb39670840405c0d99fda01e5c
adilbilal-maker <adilbilal453@gmail.com>
2026-03-09 22:03:06 +0500

    Add HTML navbar example with JavaScript styling

---
+++ CodeWithHarry HandWritten Notes Solutions/Chapter 7/1.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Navbar Example</title>
+</head>
+<body>
+
+<ul id="navbar">
+    <li>Home</li>
+    <li>About</li>
+    <li>Contact</li>
+</ul>
+
+<script>
+    let firstItem = document.querySelector("#navbar li");
+    firstItem.style.color = "red";
+</script>
+
+</body>
+</html>
